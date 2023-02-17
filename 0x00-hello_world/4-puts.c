6796a56fedea463721f9c56fa69c07ac37d22228
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-16 19:34:54 -0800

    4-puts updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - prints "programming is like building a multilingual puzzle, followed by a new line
+ * main - prints "programming is like building a multilingual
+ * puzzle, followed by new line.
  * Return: Always 0.
  */
 
@@ -10,7 +11,6 @@ int main(void)
 
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
     
