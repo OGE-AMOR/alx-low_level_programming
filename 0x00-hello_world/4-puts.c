295e335185264d7dd9fae55d69e085e21b623e4d
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-16 18:30:50 -0800

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	puts("\"programming is like building a multilinfual puzzel");
+
+	return (0);
+}
+    
+
+
