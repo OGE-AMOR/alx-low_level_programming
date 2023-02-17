02b71b860fc3bcd9ebe36c82b7628b04b163807a
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-16 18:51:04 -0800

    update on puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * main - prints "programming is like building a multilingual puzzle, followed by a new line
  * Return: Always 0.
  */
 
 int main(void)
 
 {
-	puts("\"programming is like building a multilinfual puzzel");
+	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
 }
