5ea6640183db21fcde0db8939d06f8c519443850
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-19 15:37:48 -0800

    combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num, dig;
+
+	for (num = 0; num < 9; num++)
+	{
+
+	for (dig = num + 1; dig <= 9; dig++)
+	{
+		if (dig != num)
+	{
+		putchar(num);
+		putchar(dig);
+
+		if (num == 8 && dig == 9)
+		continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
