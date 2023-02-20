802f0093cc3f439c4d042f4d76b95fcf1764ee5b
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-19 16:20:13 -0800

    possible combo of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,16 @@ int main(void)
 
 	for (num = 0; num < 9; num++)
 	{
-
-	for (dig = num + 1; dig <= 9; dig++)
-	{
-		if (dig != num)
-	{
-		putchar(num);
-		putchar(dig);
-
-		if (num == 8 && dig == 9)
-		continue;
-
-		putchar(',');
-		putchar(' ');
-	}
-	}
+		for (dig = num + 1; dig <= 9; dig++)
+		{
+			putchar(num);
+			putchar(dig);
+			if (dig != num)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 
