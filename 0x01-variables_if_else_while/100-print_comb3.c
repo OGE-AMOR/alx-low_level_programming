7544bc4f6722ffbb0f10d670f640e231637bf584
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-19 17:41:11 -0800

    correct output

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,24 @@
  */
 int main(void)
 {
+
 	int num, dig;
 
-	for (num = 0; num < 9; num++)
+	for (num = '0'; num < '9'; num++)
 	{
-		for (dig = num + 1; dig <= 9; dig++)
-		{
-			putchar(num);
-			putchar(dig);
-			if (dig != num)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+
+	for (dig = num + 1; dig <= '9'; dig++)
+	{
+	if (dig != num)
+	{
+	putchar(num);
+	putchar(dig);
+	if (num == '8' && dig == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 
