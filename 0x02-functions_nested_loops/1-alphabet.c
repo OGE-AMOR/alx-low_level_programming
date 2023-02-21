57af415261c2a53bbb37e8611c94d71de5e8be8d
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-21 14:34:13 -0800

    lowercase alpabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabet followed by new line
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+}
+
