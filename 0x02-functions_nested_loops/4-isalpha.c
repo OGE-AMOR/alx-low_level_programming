2d91743ea3ee77cfc0d2ba40f328ec9d70537e78
Mary <122952918+OGE-AMOR@users.noreply.github.com>
2023-02-22 19:02:01 -0800

    sign of a number

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - checkes for alphabetic characters
+ * @c: is the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a'; &&c <= 'z')||
+	(c >= 'A'; &&c <= 'Z'))
+	
+}
